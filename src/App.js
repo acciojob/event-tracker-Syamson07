89750d524c39617f4d600cd9870607634ccb90fa
Syam Son Penumala <syamson132000@gmail.com>
2025-06-19 20:45:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CalendarComponent from './CalendarComponent';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Event Tracker Calendar</h1>
+      <CalendarComponent />
+    </div>
+  );
+}
+
+export default App;
